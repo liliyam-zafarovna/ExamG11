ab607c443f914bc6258a14fbcf18ac7d76b89e7b
liliyam-zafarovna <apelsinqiz@gmail.com>
2025-03-03 14:18:35 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+
+namespace ExamG11;
+internal class Program
+{
+    public void MaxArray2(int[] array)
+    {
+        int number = array[0];
+        int number2 = array[0];
+        for (int i = 0; i < array.Length; i++)
+        {
+
+            if (array[i] > number)
+            {
+                number = array[i];
+            }
+        }
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > number2 && array[i] != number)
+            {
+                number2 = array[i];
+            }
+        }
+        Console.WriteLine("Arraydagi 2-eng katta son: " + number2);
+    }
+    public static void Main(string[] args)
+    {
+        int[] array = { 5, 12, 34, 13, 63, 59, 26, 94 };
+        var newObject = new Program();
+        newObject.MaxArray2(array);
+    }
+}
